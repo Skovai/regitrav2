fe7bbfe7eea15fa65c557bb4011e47caf0a28cc0
Skovai <44526656+Skovai@users.noreply.github.com>
2018-11-02 09:57:52 +0200

    Update vehicle.blade.php
    quick fix

--- resources/views/vehicle.blade.php
+++ resources/views/vehicle.blade.php
@@ -4,8 +4,8 @@
 @section('content')
     <h1>Transporto priemonės langas</h1>
     <ul>
-        <li><a href="{{action('AccountController@clientDataEditingPage')}}">Sugeneruotas laiškas</a></li>
-        <li><a href="{{action('AccountController@messagePage')}}">Valstybinių nr. registracijos langas</a></li>
+        <li><a href="{{action('AccountController@messagePage')}}">Sugeneruotas laiškas</a></li>
+        <li><a href="{{action('AccountController@clientDataEditingPage')}}">Valstybinių nr. registracijos langas</a></li>
     </ul>
 
 @endsection
