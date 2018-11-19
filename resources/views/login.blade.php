4448ff3ed5e27609ef48622e4b29a1857600a8a7
Skomantas <skovai@ktu.edu>
2018-11-19 22:51:42 +0200

    Bandymas 3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'Prisijungimas')
 @section('content')
         <h1>Prisijungimo langas Regitra V2</h1>
+        <h1>Prisijungimo langas Regitra V2</h1>
         <input type="text" placeholder="Vardas">
         <input type="password" placeholder="Slaptažodis">
         <a href="{{action('AccountController@mainPage')}}">Prisijungti</a>
