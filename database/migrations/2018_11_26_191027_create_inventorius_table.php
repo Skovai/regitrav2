873ad85eb0e2d361a51abd6f92004f72db0c37f4
Skomantas <skovai@ktu.edu>
2018-11-26 21:55:34 +0200

    Sukurtos darbuotojo posistemes lentelės

---
+++ database/migrations/2018_11_26_191027_create_inventorius_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventorius', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('pavadinimas');
+            $table->integer('serijos_numeris');
+            $table->unsignedInteger('darbuotojas_id')->index();
+            $table->foreign('darbuotojas_id')->references('id')->on('darbuotojas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventoriuses');
+    }
+}
---
+++ database/migrations/2018_11_26_193143_create_saskaita_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSaskaitaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saskaita', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('suma');
+            $table->string('paskirtis');
+            $table->date('isdavimo_data');
+            $table->dateTime('isdavimo_laikas');
+            $table->date('terminas');
+            $table->unsignedInteger('darbuotojas_id')->index();
+            $table->foreign('darbuotojas_id')->references('id')->on('darbuotojas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saskaitas');
+    }
+}
