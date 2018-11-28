93a53404bc46f0ec662d295ec6ea2d69c34108c0
Aidas Subačius <aidaso2@yahoo,com>
2018-11-28 20:28:47 +0200

    pushing Klientas

---
+++ app/Klientas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Klientas extends Model
+{
+  protected $fillable = [
+      'vardas', 'pavarde', 'asmens_kodas', 'tel_nr',
+      'miestas', 'adresas', 'gimimo_data', 'e_pastas', 'FK_Darbuotojas'
+  ];
+}
---
+++ database/migrations/2018_11_28_175236_create_klientas_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKlientasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('klientas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('miestas');
+            $table->string('vardas');
+            $table->string('pavarde');
+            $table->string('asmens_kodas');
+            $table->string('tel_nr');
+            $table->string('miestas');
+            $table->string('adresas');
+            $table->date('gimimo_data');
+            $table->string('e_pastas');
+            $table->unsignedInteger('FK_Darbuotojas')->index();
+            $table->foreign('FK_Darbuotojas')->references('id')->on('darbuotojas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('klientas');
+    }
+}
