1bccf9d91ec9ab094505233fa1ec56d3f8482863
Skomantas <skovai@ktu.edu>
2018-11-29 23:02:09 +0200

    register pradzia

--- resources/views/design.blade.php
+++ resources/views/design.blade.php
@@ -9,7 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <!-- Styles -->
         <style>
             html, body {
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,4 +7,36 @@
         <input type="text" placeholder="Vardas">
         <input type="password" placeholder="Slaptažodis">
         <a href="{{action('AccountController@mainPage')}}">Prisijungti</a>
+        <button type="button"  data-toggle="modal" data-target="#myModal">Registracija</button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Registruotis</h4>
+                    </div>
+                    <div class="modal-body">
+                        <form>
+                        <input type="text" name="vardas" id="vardas" placeholder="Vardas">
+                        <input type="text" name="pavarde" id="pavarde" placeholder="Pavarde">
+                        <input type="text" name="ak" id="ak" placeholder="Ak">
+                        <input type="tel" name="numeris" id="numeris" placeholder="Numeris">
+                        <input type="text" name="miestas" id="miestas" placeholder="Miestas">
+                        <input type="text" name="adresas" id="adresas" placeholder="Adresas">
+                        <input type="date" name="data" id="data" placeholder="Data">
+                        <input type="email" name="epastas" id="epastas" placeholder="El paštas">
+                        <button type="button">Registruotis</button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
 @endsection
